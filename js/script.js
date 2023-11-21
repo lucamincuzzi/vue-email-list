4465ce4ebb62959fe1bcf123ab16aac528e0624c
lucamincuzzi <luca.mincuzzi@outlook.it>
2023-11-21 14:53:05 +0100

    add: ul v-show condition

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ createApp({
     },
     methods: {
      generateEmails() {
-        for(i = 0; i < 9; i++) {
+        for(i = 0; i < 10; i++) {
             axios
             .get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then((resp) => {
@@ -18,7 +18,7 @@ createApp({
                 };
             })
         }
-     }   
+     },
     },
     created() {
         console.log("App renderizzata");
