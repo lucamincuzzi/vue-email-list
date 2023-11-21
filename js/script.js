a67d51b2a9c275cc8d14b4f9067e24d787860290
lucamincuzzi <luca.mincuzzi@outlook.it>
2023-11-21 16:25:08 +0100

    add: css rules for better visibility

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 const { createApp } = Vue;
 
 createApp({
-    data() {
-        return {
-            emailsList: [],
-        }
+  data() {
+    return {
+      emailsList: [],
+    };
+  },
+  methods: {
+    generateEmails() {
+      for (i = 0; i < 10; i++) {
+        axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((resp) => {
+            let rndEmail = resp.data.response;
+            if (!this.emailsList.includes(rndEmail)) {
+              this.emailsList.push(rndEmail);
+            }
+          });
+      }
     },
-    methods: {
-     generateEmails() {
-        for(i = 0; i < 10; i++) {
-            axios
-            .get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then((resp) => {
-                let rndEmail = resp.data.response;
-                if(!this.emailsList.includes(rndEmail)) {
-                    this.emailsList.push(rndEmail);
-                };
-            })
-        }
-     }   
-    },
-    created() {
-        console.log("App renderizzata");
-        this.generateEmails()
-    },
+  },
+  created() {
+    console.log("App renderizzata");
+    this.generateEmails();
+  },
 }).mount("#app");
