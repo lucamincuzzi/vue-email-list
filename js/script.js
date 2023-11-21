52f74e9ea6f8a13a5bdd7f958908f999d9793f8f
lucamincuzzi <luca.mincuzzi@outlook.it>
2023-11-21 14:39:57 +0100

    add: email list generated via API

--- js/script.js
+++ js/script.js
@@ -3,13 +3,29 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            emailsList: [this.generateEmails()],
         }
     },
     methods: {
-        
+     generateEmails() {
+        for(i = 0; i < 9; i++) {
+            axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((resp) => {
+                let rndEmail = resp.data.response;
+                if(!this.getEmailsArray.includes(rndEmail)) {
+                    this.getEmailsArray.push(rndEmail);
+                };
+            })
+        }
+     }   
     },
     created() {
         console.log("App renderizzata");
+    },
+    computed: {
+        getEmailsArray() {
+            return this.emailsList;
+        }
     },
 }).mount("#app");
