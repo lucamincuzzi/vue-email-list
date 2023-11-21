a4a99274c0ec34dd43280a0c81baf30623a9dab6
lucamincuzzi <luca.mincuzzi@outlook.it>
2023-11-21 14:05:14 +0100

    add: project init

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        
+    },
+    created() {
+        console.log("App renderizzata");
+    },
+}).mount("#app");
